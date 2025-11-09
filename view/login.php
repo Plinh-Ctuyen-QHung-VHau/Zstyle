11e4584772f6eeb774bd2d9df5930bdb536cea75
hey129 <lyphuclinh2901@gmail.com>
2025-11-09 11:21:03 +0700

    Revert "test"
    This reverts commit 20cade87db9aa7edd855cd03c04946b402e91e16.

--- view/login.php
+++ view/login.php
@@ -30,32 +30,37 @@
                 width="31"
                 height="31"
                 viewBox="0 0 31 31"
-                fill="none"> qweq
+                fill="none">
                 <path
                   d="M18 15.5C18 16.8807 16.8807 18 15.5 18C14.1193 18 13 16.8807 13 15.5C13 14.1193 14.1193 13 15.5 13C16.8807 13 18 14.1193 18 15.5Z"
                   fill="white" />
-              12312312321321312312312312
+                <circle cx="15.5" cy="15.5" r="2.5" fill="#46694F" />
+                <path
+                  d="M6.62767 14.7762L14.7759 6.62915L12.1047 3.95787L8.37018 7.69233C8.32565 7.73695 8.27275 7.77235 8.21452 7.7965C8.15629 7.82066 8.09387 7.83309 8.03082 7.83309C7.96778 7.83309 7.90536 7.82066 7.84713 7.7965C7.78889 7.77235 7.736 7.73695 7.69146 7.69233L7.01274 7.01361C6.82505 6.82592 6.82505 6.52198 7.01274 6.3349L10.7472 2.60044L8.71045 0.563075C7.96029 -0.187086 6.74452 -0.187086 5.99436 0.563075L0.562779 5.99463C-0.186781 6.74479 -0.187386 7.96055 0.562779 8.71071L6.62767 14.7762ZM30.148 7.70444C31.2838 6.5686 31.2832 4.72741 30.148 3.59157L27.4083 0.851878C26.2724 -0.283959 24.4306 -0.283959 23.2942 0.851878L20.5078 3.63759L27.3616 10.4914L30.148 7.70444ZM19.1377 5.00834L1.15552 22.988L0.019681 29.493C-0.1335 30.3703 0.63059 31.1344 1.50851 30.98L8.01417 29.8393L25.9915 11.8615L19.1377 5.00834ZM30.4374 22.2899L28.4006 20.2531L24.6661 23.9876C24.4784 24.1753 24.1745 24.1753 23.9874 23.9876L23.3087 23.3089C23.1216 23.1212 23.1216 22.8173 23.3087 22.6302L27.0432 18.8957L24.3707 16.2232L16.2224 24.3703L22.2897 30.4369C23.0399 31.1871 24.2556 31.1871 25.0058 30.4369L30.4374 25.006C31.1875 24.2558 31.1875 23.0401 30.4374 22.2899Z"
+                  fill="white" />
+              </svg>
             </a>
-          </li>quotemetawdqw
+          </li>
           <li class="app-fixed-list active">
             <a href="index.php?pg=login" class="app-fixed-link">
               <i class="fa fa-user-circle" aria-hidden="true"></i>
             </a>
           </li>
           <div class="selected-option-bg"></div>
-        </ul>dqw
+        </ul>
+      </div>
 <div class="link-mobile">
         <a href="#">Trang chủ </a>
         <i class="fa fa-chevron-right" aria-hidden="true"></i>
-        <a href="#">Áo thun</a>dqw
+        <a href="#">Áo thun</a>
       </div>
       <!-- Login -->
       <section class="login">
         <div class="container">
           <div class="login-box">
-            <div class="login-auth__login">dqwd
+            <div class="login-auth__login">
               <div class="login-title">ĐĂNG NHẬP</div>
-              <div class="login-regiter">qw
+              <div class="login-regiter">
                 Nếu bạn chưa có tài khoản, đăng ký
                 <a href="index.php?pg=register" class="regester-link"> tại đây</a>
               </div>
@@ -73,6 +78,21 @@
                   <input name="password" type="password" placeholder="Mật khẩu" value='.$_SESSION['passwordlogin'].' >
                   <i class="fa fa-eye hien"  onclick="anmatkhau()" aria-hidden="true"></i>
                 </div>
+                <div class="errform mb-unset">'.$errpassword.'</div>';
+              ?>
+              
+
+              <a href="index.php?pg=forgetpass"><div class="form-group-center">Quên mật khẩu</div></a>
+              <div class="login-button">
+                <button class="login-btn" name="login">Đăng nhập</button>
+              </div>
+              <div class="login__center">
+                <div class="form-group-center text">Hoặc đăng nhập bằng</div>
+                <div class="form-app">
+                  <div class="form-app__fb">
+                  <button class="btn_google">
+                    <ion-icon style="height:20px; width: 20px; margin-right: 10px" name="logo-facebook"></ion-icon>
+                      <span> Facebook</span>
                     </button>
                   </div>
                   <div class="form-app__google">
